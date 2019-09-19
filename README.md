048b946542d7ccb111cc451aa7108939039f4620
shaxqt <philiploesch@googlemail.com>
2019-09-19 19:01:50 +0200

    Add functions to get top songs from user

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## server env variables
 
-windows
+windows (mac/linux: use `export` instead of `SET`)
 
 ```
 SET SPOTIFY_REDIRECT=http://example.de
--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,113 @@
 const router = require('express').Router()
 const querystring = require('querystring')
-const request = require('request') // "Request" library
+const requestLib = require('request') // "Request" library
 const client_id = process.env.SPOTIFY_CLIENT_ID
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET
 const redirect_uri =
   process.env.SPOTIFY_REDIRECT || 'http://localhost:3333/callback'
 
-router.get('/login', function(req, res) {
-  /* const state = generateRandomString(16)
-  res.cookie(stateKey, state)
+/*
+ *  this function redirects the user to https://accounts.spotify.com/authorize?
+ * after loggin in and commiting access to his account, he gets redirected back to
+ * redirect_uri
  */
+router.get('/login', function(req, res) {
   // your application requests authorization
-  const scope = 'user-read-private user-read-email'
+  const scope =
+    'user-read-private user-read-email user-read-currently-playing user-read-playback-state user-top-read'
+
   res.redirect(
     'https://accounts.spotify.com/authorize?' +
       querystring.stringify({
         response_type: 'code',
         client_id: client_id,
         scope: scope,
+        // where spotify does redirect the user after loggin in
         redirect_uri: redirect_uri
-        //state: state
       })
   )
 })
 
+/*
+ * handles the callback from spotify,
+ * extracting the code (from url) to get an access token with another request
+ */
 router.get('/callback', function(req, res) {
-  console.log(req.query.token)
+  const accessToken = req.query.code
+  console.log('accessToken', accessToken)
+
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      // code to get access_token
+      code: accessToken,
+      // does not redirect, but MUST be same as in first request
+      redirect_uri: redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      Authorization:
+        'Basic ' +
+        new Buffer(client_id + ':' + client_secret).toString('base64')
+    },
+    json: true
+  }
+  // another request with given code the finaly get access token
+  requestLib.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const access_token = body.access_token
+      const refresh_token = body.refresh_token
+
+      console.log(access_token)
+
+      getTopTracks(access_token)
+      //getCurrPlayingSong(access_token)
+      //getMe(access_token)
+    } else {
+      res.send('fehler')
+    }
+  })
 })
+function getCurrPlayingSong(access_token) {
+  const options = {
+    url: 'https://api.spotify.com/v1/me/player/currently-playing',
+    headers: { Authorization: 'Bearer ' + access_token },
+    json: true
+  }
+
+  // use the access token to get curr playing song
+  requestLib.get(options, function(error, response, body) {
+    console.log('ANTWORT VON v1/me/player/currently-playing', body)
+  })
+}
+function getMe(access_token) {
+  const options = {
+    url: 'https://api.spotify.com/v1/me',
+    headers: { Authorization: 'Bearer ' + access_token },
+    json: true
+  }
+  // use the access token to access the Spotify Web API
+  requestLib.get(options, function(error, response, body) {
+    console.log('ANTWORT VON v1/me Request', body)
+  })
+}
+
+function getTopTracks(access_token) {
+  const options = {
+    url: 'https://api.spotify.com/v1/me/top/tracks',
+    headers: { Authorization: 'Bearer ' + access_token },
+    json: true
+  }
+  // use the access token to access the Spotify Web API
+  requestLib.get(options, function(error, response, body) {
+    console.log('ANTWORT VON v1/me/top/tracks Request', body)
+  })
+}
+
+module.exports = router
+
+/*   */
+
 /*
 router.get('/callback', function(req, res) {
   // your application requests refresh and access tokens
@@ -142,5 +223,3 @@ module.exports = router
   return text
 }
  */
-
-module.exports = router
