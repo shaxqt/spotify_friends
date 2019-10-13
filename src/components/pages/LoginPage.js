d6adc375e042a6370b8e5567f20b0428b16e3689
shax <philiploesch@googlemail.com>
2019-10-13 22:20:36 +0200

    Styled LoginPage, solved Buffer-Warning

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import Button from '../form/Button'
 import Main from '../utils/Main'
+import GridStyled from '../utils/GridStyled'
+import styled from 'styled-components'
 const LoginPage = props => {
   return (
-    <Main>
-      <Button
-        onClick={() => {
-          window.location = 'http://localhost:3333/auth/login'
-        }}
-        text="login"
-      />
-    </Main>
+    <MainFullWidth>
+      <GridStyled>
+        <Button
+          onClick={() => {
+            window.location = 'http://localhost:3333/auth/login'
+          }}
+          text="login"
+        />
+      </GridStyled>
+    </MainFullWidth>
   )
 }
+const MainFullWidth = styled(Main)`
+  width: 100%;
+`
 
 export default LoginPage
