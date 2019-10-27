d91a4419b2a4b9a3af08a4a067f8c90a65b8be30
shaxqt <philiploesch@googlemail.com>
2019-10-27 22:19:40 +0100

    Change filter deisgn

--- src/components/common/TimeFilter.js
+++ src/components/common/TimeFilter.js
@@ -7,32 +7,35 @@ export default function TimeFilter({ activeFilter, setFilter }) {
     <TimeFilterStyled>
       <TimeLabelStyled>Filter Top Songs</TimeLabelStyled>
       <GridStyled autoFlow="column" alignItems="center" gap="10px">
-        <FilterButtonStyled
-          active={activeFilter === 'short_term'}
-          onClick={_ => setFilter('short_term')}
-        >
-          4 weeks
-        </FilterButtonStyled>
-        <FilterButtonStyled
-          active={activeFilter === 'medium_term'}
-          onClick={_ => setFilter('medium_term')}
-        >
-          6 Month
-        </FilterButtonStyled>
-        <FilterButtonStyled
-          active={activeFilter === 'long_term'}
-          onClick={_ => setFilter('long_term')}
-        >
-          lifetime
-        </FilterButtonStyled>
+        <ButtonRowStyled>
+          <FilterButtonStyled
+            active={activeFilter === 'short_term'}
+            onClick={_ => setFilter('short_term')}
+          >
+            4 weeks
+          </FilterButtonStyled>
+          <FilterButtonStyled
+            active={activeFilter === 'medium_term'}
+            onClick={_ => setFilter('medium_term')}
+          >
+            6 Month
+          </FilterButtonStyled>
+          <FilterButtonStyled
+            active={activeFilter === 'long_term'}
+            onClick={_ => setFilter('long_term')}
+            last
+          >
+            lifetime
+          </FilterButtonStyled>
+        </ButtonRowStyled>
       </GridStyled>
     </TimeFilterStyled>
   )
 }
 
 const TimeLabelStyled = styled.label`
-  font-size: 14px;
   justify-self: start;
+  font-size: 1rem;
 `
 const TimeFilterStyled = styled.div`
   display: grid;
@@ -43,13 +46,17 @@ const TimeFilterStyled = styled.div`
   padding-bottom: 0;
 `
 
+const ButtonRowStyled = styled.div`
+  border-radius: 8px;
+  border: 1px solid #1db954;
+  overflow: hidden;
+`
 const FilterButtonStyled = styled.button`
   font-size: 14px;
-  border-style: border-box;
-  border-radius: 5px;
-  padding: 5px;
   outline: none;
+  padding: 4px 8px;
   color: ${({ active }) => (active ? '#fff' : '#1db954')};
-  border: 1px solid #1db954;
   background-color: ${({ active }) => (active ? '#1db954' : 'transparent')};
+  border: none;
+  border-right: ${({ last }) => (last ? 'none' : '1px solid #1db954')};
 `
